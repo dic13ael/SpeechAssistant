6c2e4814a7d1f26c31149f696222524cad84b170
Amanda Eliasson <dic13ael@student.lu.se>
2016-11-15 23:24:16 +0100

    Restaurant, MessageActivity

--- app/src/main/java/com/tnx097/speechassistant/MessageActivity.java
+++ app/src/main/java/com/tnx097/speechassistant/MessageActivity.java
@@ -1,24 +1,17 @@
 package com.tnx097.speechassistant;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class RestaurantActivity extends AppCompatActivity {
+public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant);
+        setContentView(R.layout.activity_message);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-    public void MessageClick1(View v){
-        Intent intent = new Intent(this,RestaurantActivity.class);
-        startActivity(intent);
-
     }
 }
