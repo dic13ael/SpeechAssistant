378a9849322a801430b5cfa6ffbc9a3d5f78cf2d
tesskl <tessie_bell@hotmail.com>
2016-11-16 19:14:23 +0100

    Igenkänning av tal tillagt.

--- app/src/main/java/com/tnx097/speechassistant/ListenActivity.java
+++ app/src/main/java/com/tnx097/speechassistant/ListenActivity.java
@@ -1,12 +1,20 @@
 package com.tnx097.speechassistant;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class ListenActivity extends AppCompatActivity {
@@ -18,10 +26,19 @@ private TextView textview;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listen);
-        setMessage("Följ med mig så ska jag visa ert bord!");
+       /* setMessage("Följ med mig så ska jag visa ert bord!");
         textview = (TextView)findViewById(R.id.text_listen);
         textview.setText("");
-        displayText(textview);
+        displayText(textview);*/
+        textview = (TextView)findViewById(R.id.text_listen);
+        listen();
+
+        final Button button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                listen();
+            }
+        });
 
 
     }
@@ -38,6 +55,37 @@ private TextView textview;
 
         }
     }
+
+    public void listen(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Säg något!");
+        try {
+
+            startActivityForResult(intent, 100);
+        } catch (ActivityNotFoundException a) {
+
+            Toast.makeText(getApplicationContext(),
+                    "Din mobil stödjer inte igenkänning av tal!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+public void onActivityResult(int request_code, int result_code, Intent i){
+
+    super.onActivityResult(request_code, result_code, i);
+    switch(request_code){
+        case 100: if (result_code == RESULT_OK && i != null){
+
+            ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            textview.setText(result.get(0));
+        }
+            break;
+    }
+
+}
 
 
 }
