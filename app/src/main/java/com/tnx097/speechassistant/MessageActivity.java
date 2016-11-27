3bd3a244191e784b9b01ddef5dec504dcd1008b3
tesskl <tessie_bell@hotmail.com>
2016-11-27 11:51:03 +0100

    spela upp blir röd + vibration

--- app/src/main/java/com/tnx097/speechassistant/MessageActivity.java
+++ app/src/main/java/com/tnx097/speechassistant/MessageActivity.java
@@ -1,6 +1,9 @@
 package com.tnx097.speechassistant;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +25,9 @@ import java.util.Locale;
 public class MessageActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private String words;
     private TextToSpeech myTTS;
+    private boolean isSpeaking = false;
+    private Vibrator vib;
+    private CountDownTimer timer;
 
 
     @Override
@@ -31,13 +37,14 @@ public class MessageActivity extends AppCompatActivity implements TextToSpeech.O
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+        vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
-        //button animation
+      /*//button animation
         final Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
         animation.setDuration(250); // duration - half a second
         animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
 
-        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in*/
 
         Intent checkTTSIntent = new Intent();
         checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
@@ -46,19 +53,52 @@ public class MessageActivity extends AppCompatActivity implements TextToSpeech.O
         final Button button = (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+              if (timer != null) { timer.cancel();}
 
+                if (!isSpeaking){
+
+                isSpeaking = true;
                 EditText enteredText = (EditText) findViewById(R.id.meddelandeText);
                 words = enteredText.getText().toString();
+                int sec = 1 + words.length() / 6;
 
-
-                // Repeat animation for some time
+             /*   // Repeat animation for some time
                 int sec = 1 + words.length() / 6;
                 animation.setRepeatCount(sec);
-                button.startAnimation(animation);
+                button.startAnimation(animation);*/
 
+                button.setText("Stoppa");
+                button.setBackgroundColor(getResources().getColor(R.color.red));
+                vib.vibrate(50);
                 speak(words);
 
+               timer = new CountDownTimer(sec*400, 50) {
 
+                    @Override
+                    public void onTick(long arg0) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        button.setBackgroundColor(getResources().getColor(R.color.buttonGreen));
+                        button.setText("Spela upp");
+                        vib.vibrate(50);
+                        isSpeaking = false;
+                    }
+                }.start();
+
+            }
+
+            else {
+
+                    speak(" ");
+                    button.setBackgroundColor(getResources().getColor(R.color.buttonGreen));
+                    button.setText("Spela upp");
+                    vib.vibrate(50);
+                    isSpeaking = false;
+                }
             }
         });
 
